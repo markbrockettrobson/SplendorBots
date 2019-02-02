3a5bcd57ac6ba1b883c39b37bcbaf2349cdb22a1
Mark Robson <21517037@student.uwa.edu.au>
2019-02-02 17:38:25 +1100

    edited .gitignore added file structure added requierments.txt

---
+++ splendor_sim/interfaces/data_types/i_coin_reserve.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class ICoinReserve(ABC):
+
+    def __init__(self, color_names: List[str], max_number_of_coin: int):
+        raise NotImplemented
+
+    def __str__(self)-> str:
+        raise NotImplemented
+
+    @abstractmethod
+    def print_std_out(self):
+        raise NotImplemented
