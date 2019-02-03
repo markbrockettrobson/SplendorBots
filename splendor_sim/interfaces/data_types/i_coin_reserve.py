a60a6af99062b436f134b621013f921b02a3eb63
markbrockettrobson <markbrockettrobson@gmaillcom>
2019-02-03 23:06:41 +1100

    edited old tests added new functions to coin reserve

--- splendor_sim/interfaces/data_types/i_coin_reserve.py
+++ splendor_sim/interfaces/data_types/i_coin_reserve.py
@@ -1,15 +1,25 @@
 from abc import ABC, abstractmethod
 from typing import List
 
+from splendor_sim.interfaces.collection_rules.i_collection_rule import ICollectionRule
+
 
 class ICoinReserve(ABC):
 
     def __init__(self, color_names: List[str], max_number_of_coin: int):
-        super(ICoinReserve, self).__init__()
+        pass
 
     def __str__(self) -> str:
         pass
 
     @abstractmethod
     def print_std_out(self):
+        pass
+
+    @abstractmethod
+    def add_rule(self, rule: ICollectionRule):
+        pass
+
+    @abstractmethod
+    def get_rules(self) -> List[ICollectionRule]:
         pass
