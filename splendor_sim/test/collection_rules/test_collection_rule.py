002ffa5bd5506968e9f83b44a0fa0aeb35314e13
markbrockettrobson <markbrockettrobson@gmail.com>
2019-02-09 22:06:38 +1100

    new test format on current tests

--- splendor_sim/test/collection_rules/test_collection_rule.py
+++ splendor_sim/test/collection_rules/test_collection_rule.py
@@ -1,43 +1,38 @@
+import unittest
 from splendor_sim.collection_rules.collection_rule import CollectionRule
 
 
-class TestCollectionRule:
+class TestCollectionRule(unittest.TestCase):
 
-    @staticmethod
-    def setup():
-        collection_rule = CollectionRule()
-        return collection_rule
+    def setUp(self):
+        self._collection_rule = CollectionRule()
 
     def test_collection_rule_str(self):
         # ASSEMBLE
-        # ACT
-        collection_rule = self.setup()
         expected = "CollectionRule super class"
 
         # ACT
-        real = str(collection_rule)
+        real = str(self._collection_rule)
 
         # ASSERT
         assert expected == real
 
     def test_collection_rule_get_description(self):
         # ASSEMBLE
-        collection_rule = self.setup()
         expected = "CollectionRule super class"
 
         # ACT
-        real = collection_rule.get_description()
+        real = self._collection_rule.get_description()
 
         # ASSERT
         assert expected == real
 
     def test_collection_rule_is_valid(self):
         # ASSEMBLE
-        collection_rule = self.setup()
         expected = False
 
         # ACT
-        real = collection_rule.is_valid(None, None)
+        real = self._collection_rule.is_valid(None, None)
 
         # ASSERT
         assert expected == real
