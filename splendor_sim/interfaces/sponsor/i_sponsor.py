659230ba7b100b03187c712189384601f95f664e
markbrockettrobson <markbrockettrobson@gmail.com>
2019-03-03 22:36:46 +1100

    sponsor interface and tests

---
+++ splendor_sim/interfaces/sponsor/i_sponsor.py
@@ -0,0 +1,32 @@
+import abc
+import typing
+
+import splendor_sim.interfaces.coin.i_coin_type as i_coin_type
+
+
+class ISponsor(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self,
+                 victory_points: int,
+                 cost: typing.Dict[i_coin_type.ICoinType, int]):
+        """
+
+        :param victory_points:
+        :param cost:
+        """
+
+    @abc.abstractmethod
+    def get_victory_points(self) -> str:
+        """
+
+        :return: the number of victory points given by the sponsor
+                 <int>
+        """
+
+    @abc.abstractmethod
+    def get_cost(self) -> typing.Dict[i_coin_type.ICoinType, int]:
+        """
+
+        :return: the cost of the sponsor
+                 <typing.Dict[i_coin_type.ICoinType, int]>
+        """
---
+++ splendor_sim/test/sponsor/test_sponsor.py
@@ -0,0 +1,74 @@
+import copy
+import unittest
+import unittest.mock as mock
+import splendor_sim.interfaces.coin.i_coin_type as i_coin_type
+import splendor_sim.sponsor.sponsor as sponsor
+
+
+class TestSponsor(unittest.TestCase):
+
+    def setUp(self):
+        self._victory_points = 3
+        self._mock_coin_type_list = [mock.create_autospec(spec=i_coin_type.ICoinType,spec_set=True)for _ in range(3)]
+        self._cost = {}
+        for card in self._mock_coin_type_list:
+            self._cost[card] = 2
+
+    def test_sponsor_init_vaild(self):
+        # Arrange
+        # Act
+        test_sponsor = sponsor.Sponsor(self._victory_points,self._cost)
+        # Assert
+        self.assertEqual(test_sponsor.get_victory_points(), self._victory_points)
+        self.assertEqual(test_sponsor.get_cost(), self._cost)
+
+    def test_sponsor_init_invalid_victory_points(self):
+        # Arrange
+        self._victory_points = 0
+        # Act
+        # Assert
+        with self.assertRaises(ValueError):
+            _ = sponsor.Sponsor(self._victory_points, self._cost)
+
+    def test_sponsor_init_invalid_cost(self):
+        # Arrange
+        self._cost = {}
+        # Act
+        # Assert
+        with self.assertRaises(ValueError):
+            _ = sponsor.Sponsor(self._victory_points, self._cost)
+
+    def test_sponsor_init_cost_post_init_immutability(self):
+        # Arrange
+        test_sponsor = sponsor.Sponsor(self._victory_points, self._cost)
+        pre_mutation = copy.copy(self._cost)
+        # Act
+        self._cost.pop(list(self._cost.keys())[0])
+        # Assert
+        self.assertEqual(test_sponsor.get_cost(), pre_mutation)
+
+    def test_sponsor_get_victory_points(self):
+        # Arrange
+        test_sponsor = sponsor.Sponsor(self._victory_points, self._cost)
+        # Act
+        # Assert
+        self.assertEqual(test_sponsor.get_victory_points(), self._victory_points)
+
+    def test_sponsor_get_cost(self):
+        # Arrange
+        test_sponsor = sponsor.Sponsor(self._victory_points, self._cost)
+        # Act
+        # Assert
+        self.assertEqual(test_sponsor.get_cost(), self._cost)
+
+    def test_sponsor_get_cost_immutability(self):
+        # Arrange
+        test_sponsor = sponsor.Sponsor(self._victory_points, self._cost)
+        return_value = test_sponsor.get_cost()
+        pre_mutation = copy.copy(return_value)
+        # Act
+        return_value.pop(list(return_value)[0])
+        # Assert
+        self.assertEqual(test_sponsor.get_cost(), pre_mutation)
+
+
