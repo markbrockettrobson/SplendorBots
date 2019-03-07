8107a2eb5d513fa5a88cb3ab2ba2aa883218e401
markbrockettrobson <markbrockettrobson@gmail.com>
2019-03-07 21:51:08 +1100

    added player coin inventory

---
+++ splendor_sim/interfaces/player/i_player_coin_inventory.py
@@ -0,0 +1,51 @@
+import abc
+import typing
+
+import splendor_sim.interfaces.coin.i_coin_type_manager as i_coin_type_manager
+import splendor_sim.interfaces.coin.i_coin_type as i_coin_type
+
+
+class IPlayerCoinInventory(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self, coin_type_manager: i_coin_type_manager.ICoinTypeManager):
+        """
+
+        :param coin_type_manager: holds all the coin types
+               <i_coin_type_manager.ICoinTypeManager>
+        """
+
+    @abc.abstractmethod
+    def get_coins(self) -> typing.Dict[i_coin_type.ICoinType, int]:
+        """
+
+        :return: dictionary mapping each coin type in the manager to the amount the player has
+                 <typing.Dict[i_coin_type.ICoinType, int]>
+        """
+
+    @abc.abstractmethod
+    def has_minimum(self, minimum: typing.Dict[i_coin_type.ICoinType, int]) -> bool:
+        """
+
+        :param minimum: dictionary mapping each coin type in the manager to the minimum amount to test against
+               <typing.Dict[i_coin_type.ICoinType, int]>
+        :return: true if the player has at least minimum coins of each type
+                 <bool>
+        """
+
+    @abc.abstractmethod
+    def add_coins(self, coins: typing.Dict[i_coin_type.ICoinType, int]) -> None:
+        """
+
+        :param coins:  dictionary mapping each coin type in the manager to the amount of coins to add
+               <typing.Dict[i_coin_type.ICoinType, int]>
+        :return: None
+        """
+
+    @abc.abstractmethod
+    def remove_coins(self, coins: typing.Dict[i_coin_type.ICoinType, int]) -> None:
+        """
+
+        :param coins: dictionary mapping each coin type in the manager to the amount to remove
+               <typing.Dict[i_coin_type.ICoinType, int]>
+        :return: None
+        """
---
+++ splendor_sim/src/player/player_coin_inventory.py
@@ -0,0 +1,49 @@
+import copy
+import typing
+
+import splendor_sim.interfaces.player.i_player_coin_inventory as i_player_coin_inventory
+import splendor_sim.interfaces.coin.i_coin_type_manager as i_coin_type_manager
+import splendor_sim.interfaces.coin.i_coin_type as i_coin_type
+
+
+class PlayerCoinInventory(i_player_coin_inventory.IPlayerCoinInventory):
+
+    def __init__(self, coin_type_manager: i_coin_type_manager.ICoinTypeManager):
+        self._coin_type_manager = coin_type_manager
+        self._coin_list = self._coin_type_manager.get_coin_list()
+        self._max_coin_size = {coin: coin.get_total_number() for coin in self._coin_list} \
+            # type: typing.Dict[i_coin_type.ICoinType, int]
+        self._current_coins = {coin: coin.get_total_number() for coin in self._coin_list} \
+            # type: typing.Dict[i_coin_type.ICoinType, int]
+
+    def get_coins_remaining(self) -> typing.Dict[i_coin_type.ICoinType, int]:
+        return copy.copy(self._current_coins)
+
+    def get_coins(self) -> typing.Dict[i_coin_type.ICoinType, int]:
+        return copy.copy(self._max_coin_size)
+
+    def has_minimum(self, minimum: typing.Dict[i_coin_type.ICoinType, int]) -> bool:
+        self._validate_input_dictionary(minimum)
+        for coin, value in minimum.items():
+            if self._current_coins[coin] < value:
+                return False
+        return True
+
+    def add_coins(self, coins: typing.Dict[i_coin_type.ICoinType, int]) -> None:
+        self._validate_input_dictionary(coins)
+        for coin, value in coins.items():
+            if self._current_coins[coin] + value > self._max_coin_size[coin]:
+                raise ValueError("add would set " + str(coin) + " above its max")
+            self._current_coins[coin] += value
+
+    def remove_coins(self, coins: typing.Dict[i_coin_type.ICoinType, int]) -> None:
+        self._validate_input_dictionary(coins)
+        for coin, value in coins.items():
+            if self._current_coins[coin] - value < 0:
+                raise ValueError("removed would set " + str(coin) + " bellow zero")
+            self._current_coins[coin] -= value
+
+    def _validate_input_dictionary(self, dictionary: typing.Dict[i_coin_type.ICoinType, int]) -> None:
+        for key in dictionary:
+            if key not in self._coin_list:
+                raise ValueError(str(key) + "is not a coin type in this Reserve")
