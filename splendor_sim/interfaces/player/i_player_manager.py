2dafd572cc8b0e12a8eef1058090b3a99fc2c3d4
markbrockettrobson <markbrockettrobson@gmail.com>
2019-03-12 00:38:02 +1100

    document + mypy types + lint

--- splendor_sim/interfaces/player/i_player_manager.py
+++ splendor_sim/interfaces/player/i_player_manager.py
@@ -6,36 +6,36 @@ import splendor_sim.interfaces.player.i_player as i_player
 class IPlayerManager(abc.ABC):
 
     @abc.abstractmethod
-    def __init__(self, player_list: typing.Set[i_player.IPlayer]):
+    def __init__(self, player_set: typing.Set[i_player.IPlayer]):
         """
 
-        :param player_list:
-        """
-
-    @abc.abstractmethod
-    def get_player_set(self):
-        """
-
-        :return:
+        :param player_set: a set of all players to manage
         """
 
     @abc.abstractmethod
-    def get_current_player(self):
+    def get_player_set(self) -> typing.Set[i_player.IPlayer]:
         """
 
-        :return:
-        """
-
-    @abc.abstractmethod
-    def next_players_turn(self):
-        """
-
-        :return:
+        :return: a set of all players in the manager
         """
 
     @abc.abstractmethod
-    def get_turn_number(self):
+    def get_current_player(self) -> i_player.IPlayer:
         """
 
-        :return:
+        :return: the current player
+        """
+
+    @abc.abstractmethod
+    def next_players_turn(self) -> i_player.IPlayer:
+        """
+
+        :return: move to the next player and return the new current player
+        """
+
+    @abc.abstractmethod
+    def get_turn_number(self) -> int:
+        """
+
+        :return: return the turn number
         """
