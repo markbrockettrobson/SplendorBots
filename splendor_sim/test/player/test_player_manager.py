3d510e30025a9ab7b0b0399e803b899ddd11149d
markbrockettrobson <markbrockettrobson@gmail.com>
2019-03-11 23:47:42 +1100

    tests coded

--- splendor_sim/test/player/test_player_manager.py
+++ splendor_sim/test/player/test_player_manager.py
@@ -1,70 +1,84 @@
 import unittest
 import unittest.mock as mock
-import typing
+
 import splendor_sim.interfaces.player.i_player as i_player
+import splendor_sim.src.player.player_manager as player_manager
 
 
-class IPlayer(unittest.TestCase):
+class TestPlayerManager(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self._number_of_players = 4
+        self._mock_player_set = \
+            {
+                mock.create_autospec(spec=i_player.IPlayer, spec_set=True)
+                for _ in range(self._number_of_players)
+            }
 
     def test_player_manager_init_valid(self):
         # Arrange
         # Act
+        test_player_manager = player_manager.PlayerManger(self._mock_player_set)
         # Assert
-        pass
-
-    def test_player_manager_init_invalid_repeat_player(self):
-        # Arrange
-        # Act
-        # Assert
+        self.assertEqual(test_player_manager.get_player_set(), self._mock_player_set)
         pass
 
     def test_player_manager_init_invalid_repeat_player_immutability(self):
         # Arrange
+        test_player_manager = player_manager.PlayerManger(self._mock_player_set)
         # Act
+        self._mock_player_set.pop()
         # Assert
-        pass
+        self.assertEqual(test_player_manager.get_player_set(), self._mock_player_set)
 
     def test_player_manager_get_player_set(self):
         # Arrange
+        test_player_manager = player_manager.PlayerManger(self._mock_player_set)
         # Act
+        self._mock_player_set.pop()
         # Assert
-        pass
+        self.assertEqual(test_player_manager.get_player_set(), self._mock_player_set)
 
     def test_player_manager_get_current_player(self):
         # Arrange
+        test_player_manager = player_manager.PlayerManger(self._mock_player_set)
         # Act
         # Assert
-        pass
+        self.assertIn(test_player_manager.get_current_player(), self._mock_player_set)
 
     def test_player_manager_get_current_player_multiple_calls(self):
         # Arrange
+        test_player_manager = player_manager.PlayerManger(self._mock_player_set)
         # Act
+        last_call = test_player_manager.get_current_player()
         # Assert
-        pass
-
-    def test_player_manager_get_current(self):
-        # Arrange
-        # Act
-        # Assert
-        pass
+        self.assertEqual(test_player_manager.get_current_player(), last_call)
 
     def test_player_manager_next_players_turn(self):
         # Arrange
+        test_player_manager = player_manager.PlayerManger(self._mock_player_set)
         # Act
+        last_call = test_player_manager.get_current_player()
         # Assert
-        pass
+        self.assertNotEqual(test_player_manager.next_players_turn(), last_call)
 
     def test_player_manager_next_players_turn_rap_around(self):
         # Arrange
+        test_player_manager = player_manager.PlayerManger(self._mock_player_set)
         # Act
+        player_list = []
+        for i in range(self._number_of_players * 2):
+            player_list.append(test_player_manager.get_current_player())
         # Assert
-        pass
+        for player in player_list:
+            self.assertEqual(test_player_manager.next_players_turn(), player)
 
     def test_player_manager_get_turn_number(self):
         # Arrange
+        test_player_manager = player_manager.PlayerManger(self._mock_player_set)
         # Act
-        # Assert
-        pass
+        for i in range(20):
+            for j in range(self._number_of_players * i):
+                test_player_manager.get_current_player()
+            # Assert
+            self.assertEqual(test_player_manager.get_turn_number(), i)
