663faa06f1343c7c127eb6d34b2d67fcdf0bdf47
markbrockettrobson <markbrockettrobson@gmail.com>
2019-03-12 20:54:38 +1100

    added game state src

--- splendor_sim/src/game_state/game_state.py
+++ splendor_sim/src/game_state/game_state.py
@@ -7,4 +7,27 @@ import splendor_sim.interfaces.sponsor.i_sponsor_reserve as i_sponsor_reserve
 
 
 class GameState(abc.ABC):
-    pass
+
+    def __init__(
+            self,
+            player_manager: i_player_manager.IPlayerManager,
+            coin_reserve: i_coin_reserve.ICoinReserve,
+            card_reserve: i_card_reserve.ICardReserve,
+            sponsor_reserve: i_sponsor_reserve.ISponsorReserve
+    ):
+        self._player_manager = player_manager
+        self._coin_reserve = coin_reserve
+        self._card_reserve = card_reserve
+        self._sponsor_reserve = sponsor_reserve
+
+    def get_player_manager(self) -> i_player_manager.IPlayerManager:
+        return self._player_manager
+
+    def get_coin_reserve(self) -> i_coin_reserve.ICoinReserve:
+        return self._coin_reserve
+
+    def get_card_reserve(self) -> i_card_reserve.ICardReserve:
+        return self._card_reserve
+
+    def get_sponsor_reserve(self) -> i_sponsor_reserve.ISponsorReserve:
+        return self._sponsor_reserve
