bce12b75af5810b77925bc2418ca996b2859deb5
markbrockettrobson <markbrockettrobson@gmail.com>
2019-03-13 23:55:57 +1100

    coin manager. moved to set from lists

--- splendor_sim/interfaces/card/i_card_manager.py
+++ splendor_sim/interfaces/card/i_card_manager.py
@@ -7,36 +7,36 @@ class ICardManager(abc.ABC):
     @abc.abstractmethod
     def __init__(
             self,
-            card_list: typing.List[i_card.ICard]
+            card_set: typing.Set[i_card.ICard]
     ):
         """
 
-        :param card_list: a list of cards in the game
-               <typing.List[i_card.ICoinType]>
+        :param card_set: a set of cards in the game
+               <typing.Set[i_card.ICoinType]>
         """
 
     @abc.abstractmethod
-    def get_tiers(self) -> typing.List[int]:
+    def get_tiers(self) -> typing.Set[int]:
         """
 
-        :return: a list of all tiers
-                 <typing.List[int]>
-        """
-
-    @abc.abstractmethod
-    def get_card_list(self) -> typing.List[i_card.ICard]:
-        """
-
-        :return: a list of all cards
-                <typing.List[i_card.ICoinType]>
+        :return: a set of all tiers
+                 <typing.Set[int]>
         """
 
     @abc.abstractmethod
-    def get_card_tier(self, tier: int) -> typing.List[i_card.ICard]:
+    def get_card_set(self) -> typing.Set[i_card.ICard]:
+        """
+
+        :return: a set of all cards
+                <typing.Set[i_card.ICoinType]>
+        """
+
+    @abc.abstractmethod
+    def get_card_tier(self, tier: int) -> typing.Set[i_card.ICard]:
         """
 
         :param tier: the tier to select
                <int>
-        :return: a list of all cards in the given tier
-                 <typing.List[i_card.ICoinType]>
+        :return: a set of all cards in the given tier
+                 <typing.Set[i_card.ICoinType]>
         """
