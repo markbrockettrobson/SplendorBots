ca8c57714c9f4ad6ca2c3acd2a06ab61dedcd7d8
Mark Brockett Robson <markbrockettrobson@gmail.com>
2019-04-06 15:31:27 +1100

    add payment_manager to game state

--- splendor_sim/src/game_state/game_state.py
+++ splendor_sim/src/game_state/game_state.py
@@ -4,6 +4,7 @@ import splendor_sim.interfaces.player.i_player_manager as i_player_manager
 import splendor_sim.interfaces.coin.i_coin_reserve as i_coin_reserve
 import splendor_sim.interfaces.card.i_card_reserve as i_card_reserve
 import splendor_sim.interfaces.sponsor.i_sponsor_reserve as i_sponsor_reserve
+import splendor_sim.interfaces.coin.i_payment_manager as i_payment_manager
 
 
 class GameState(abc.ABC):
@@ -13,12 +14,14 @@ class GameState(abc.ABC):
             player_manager: i_player_manager.IPlayerManager,
             coin_reserve: i_coin_reserve.ICoinReserve,
             card_reserve: i_card_reserve.ICardReserve,
-            sponsor_reserve: i_sponsor_reserve.ISponsorReserve
+            sponsor_reserve: i_sponsor_reserve.ISponsorReserve,
+            payment_manager: i_payment_manager.IPaymentManager
     ):
         self._player_manager = player_manager
         self._coin_reserve = coin_reserve
         self._card_reserve = card_reserve
         self._sponsor_reserve = sponsor_reserve
+        self._payment_manager = payment_manager
 
     def get_player_manager(self) -> i_player_manager.IPlayerManager:
         return self._player_manager
@@ -31,3 +34,6 @@ class GameState(abc.ABC):
 
     def get_sponsor_reserve(self) -> i_sponsor_reserve.ISponsorReserve:
         return self._sponsor_reserve
+
+    def get_payment_manager(self) -> i_payment_manager.IPaymentManager:
+        return self._payment_manager
