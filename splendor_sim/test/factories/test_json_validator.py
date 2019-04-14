96af3f67441974c355da050ca0b86b0c520a9400
Mark Brockett Robson <markbrockettrobson@gmail.com>
2019-04-14 15:15:29 +1000

    refactored json validators

--- splendor_sim/test/factories/test_json_validator.py
+++ splendor_sim/test/factories/test_json_validator.py
@@ -1,52 +1,68 @@
 import unittest
-import unittest.mock as mock
 
 import splendor_sim.src.factories.json_validator as json_validator
 
 
 class TestJsonValidator(unittest.TestCase):
     def setUp(self):
-        self._validator_patcher = mock.patch('cerberus.validator', spec=True)
-        self._mock_validator = self._validator_patcher.start()
-        self.addCleanup(self._validator_patcher.stop)
-
         self._schema = {
             'name': {
                 'type': 'string'
             },
             'age': {
-                'type': 'string',
-                'min': 10
+                'type': 'integer',
+                'min': 10,
+                'required': True,
             }
         }
 
         self._json = {
             'name': 'mark',
-            'age': '10'
+            'age': 10
         }
 
-    def test_validate_json_schema(self):
+    def test_validate_json(self):
         # Arrange
+        test_json_validator = json_validator.JsonValidator(self._schema)
         # Act
         # Assert
         self.assertTrue(
-            json_validator.JsonValidator.validate_json_schema(
-                self._json,
-                self._schema
-            )
+            test_json_validator.validate_json(self._json)
         )
 
-    def test_validate_json_schema_false(self):
+    def test_validate_json_true_missing_non_required_field(self):
+        # Arrange
+        self._json = {
+            'age': 10
+        }
+        test_json_validator = json_validator.JsonValidator(self._schema)
+        # Act
+        # Assert
+        self.assertTrue(
+            test_json_validator.validate_json(self._json)
+        )
+
+    def test_validate_json_false_incorrect_type(self):
         # Arrange
         self._json = {
             'name': 'mark',
-            'age': 10
+            'age': '10'
         }
+        test_json_validator = json_validator.JsonValidator(self._schema)
         # Act
         # Assert
         self.assertFalse(
-            json_validator.JsonValidator.validate_json_schema(
-                self._json,
-                self._schema
-            )
+            test_json_validator.validate_json(self._json)
+        )
+
+    def test_validate_json_false_missing_required_field(self):
+        # Arrange
+        self._json = {
+            'name': 'mark'
+        }
+        test_json_validator = json_validator.JsonValidator(self._schema)
+        # Act
+        # Assert
+        self.assertFalse(
+            test_json_validator.validate_json(self._json)
         )
