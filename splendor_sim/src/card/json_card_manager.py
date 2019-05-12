2bf984315d81473bd177580d7a6ab1ac17705d26
Mark Brockett Robson <markbrockettrobson@gmail.com>
2019-05-12 21:38:07 +1000

    json buildable card manager + tests

---
+++ splendor_sim/src/card/json_card_manager.py
@@ -0,0 +1,45 @@
+import copy
+import typing
+
+import splendor_sim.interfaces.game_state.i_game_state as i_game_state
+import splendor_sim.interfaces.factories.i_json_buildable_object as i_json_buildable_object
+import splendor_sim.interfaces.card.i_card as i_card
+import splendor_sim.src.factories.json_validator as json_validator
+import splendor_sim.src.factories.json_schemas as json_schemas
+import splendor_sim.src.card.card_manager as card_manager
+import splendor_sim.src.card.json_card as json_card
+
+
+class JsonCardManager(card_manager.CardManager, i_json_buildable_object.IJsonBuildableObject):
+
+    _JSON_VALIDATOR = json_validator.JsonValidator(json_schemas.JSON_CARD_MANAGER_SCHEMA)
+
+    def __init__(
+            self,
+            card_set: typing.Set[i_card.ICard]
+    ):
+        super(JsonCardManager, self).__init__(
+            card_set
+        )
+
+    @classmethod
+    def build_from_json(
+            cls,
+            json: typing.Dict,
+            incomplete_game_state: i_game_state.IGameState
+    ):
+        if not cls._JSON_VALIDATOR.validate_json(json):
+            raise ValueError("Json does not meet schema")
+
+        card_set = set()
+        for card_json in json["cards"]:
+            card_set.add(json_card.JsonCard.build_from_json(card_json, i_game_state))
+
+        return cls(
+            card_set
+        )
+
+    @staticmethod
+    def get_json_schema() -> typing.Dict:
+        return copy.deepcopy(json_schemas.JSON_CARD_MANAGER_SCHEMA)
+
